8bb7ef8f36bd4cf98d5e7e315ea7667b15372db2
Christianey <eyubechristian@gmail.com>
2020-10-20 03:47:07 +0100

    working on collection components

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './collection-item.styles.scss';
+
+const CollectionItem = (params) => (
+    <h1>okay here we go again</h1>
+)
+
+
+export default CollectionItem;
