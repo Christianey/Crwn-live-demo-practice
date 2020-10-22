247fd1039db1d9382352cdf710369f36b1736287
Christianey <eyubechristian@gmail.com>
2020-10-22 16:18:12 +0100

    added sign and form components with button and styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Header/>
       <Switch>
           <Route exact path='/' component={HomePage}></Route>
+          <Route exact path='/signin' component={SignInAndSignUpPage}></Route>
           <Route path='/shop' component={ShopPage}></Route>
       </Switch>
     </div>
