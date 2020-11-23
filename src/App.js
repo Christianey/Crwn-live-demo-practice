88f7becf217533e514649de735140e3abadcef61
Christianey <eyubechristian@gmail.com>
2020-11-23 10:57:28 +0100

    implemented pulling of shop data from firestore. And also withSpinnerHOC

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import {
+   auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions'
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { createStructuredSelector } from 'reselect';
 import CheckoutPage from './pages/checkout/checkout.component';
+import { selectCollectionsForPreview } from './redux/shop/shop.selector';
 
 class App extends Component {
   componentDidMount() {
@@ -31,8 +33,6 @@ class App extends Component {
           })
         });
       }
-
-      setCurrentUser(userAuth)
     });
   }
 
@@ -57,7 +57,8 @@ class App extends Component {
 }
 
 const mapStateToProps =  createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  collectionsArray: selectCollectionsForPreview
 })
 
 const mapDispatchToProps = dispatch => ({
